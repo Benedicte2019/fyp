33063fba1176ee44a2b085979fb8fa1fde2c9135
Benedicte <benefatia@gmail.com>
2020-07-24 16:29:15 +0300

    mydetector folder added

---
+++ mydetector/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+# Register your models here.
+
+#registering views
+#from .models import Patients
+#admin.site.register(Patients)
+
+#from .models import Doctors
+#admin.site.register(Doctors)
+
+#from .models import Measurements
+#admin.site.register(Measurements)
+
+#from .models import Visitors
+#admin.site.register(Visitors)
